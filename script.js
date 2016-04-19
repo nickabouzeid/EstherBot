7f45ae276bb586f8acfe1a5aa80f0779f95a31a6
Nick Abouzeid <nickzeid@gmail.com>
2016-04-19 19:38:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Hey! So you want to learn about Nick? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
